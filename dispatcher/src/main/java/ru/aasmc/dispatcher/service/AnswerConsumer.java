800d7a8fe7977b1200737507e1f0d10832c68520
Alex Alexeyev <aasmc@yandex.ru>
2022-12-31 15:41:36 +0300

    Setup and configure rabbitmq

---
+++ dispatcher/src/main/java/ru/aasmc/dispatcher/service/AnswerConsumer.java
@@ -0,0 +1,7 @@
+package ru.aasmc.dispatcher.service;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public interface AnswerConsumer {
+    void consume(SendMessage sendMessage);
+}
---
+++ dispatcher/src/main/java/ru/aasmc/dispatcher/service/UpdateProducer.java
@@ -0,0 +1,7 @@
+package ru.aasmc.dispatcher.service;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public interface UpdateProducer {
+    void produce(String rabbitQueue, Update update);
+}
