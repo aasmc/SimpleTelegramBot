9975ce2b3c2208fe1ab5a949c3e1620a82352347
Alex Alexeyev <aasmc@yandex.ru>
2022-12-31 19:46:50 +0300

    Add logic to handle text commands from chat

--- node/src/main/java/ru/aasmc/service/impl/MainServiceImpl.java
+++ node/src/main/java/ru/aasmc/service/impl/MainServiceImpl.java
@@ -12,6 +12,7 @@ import ru.aasmc.dao.RawDataDAO;
 import ru.aasmc.entity.UserState;
 import ru.aasmc.service.MainService;
 import ru.aasmc.service.ProducerService;
+import ru.aasmc.service.enums.ServiceCommands;
 
 @Log4j
 @Service
@@ -32,18 +33,107 @@ public class MainServiceImpl implements MainService {
     @Override
     public void processTextMessage(Update update) {
         saveRawData(update);
-        var textMessage = update.getMessage();
-        var telegramUser = textMessage.getFrom();
-        var appUser = findOrSaveAppUser(telegramUser);
 
-        var message = update.getMessage();
+        var appUser = findOrSaveAppUser(update);
+        var userState = appUser.getState();
+        var text = update.getMessage().getText();
+        var output = "";
+
+        if (ServiceCommands.CANCEL.equals(text)) {
+            output = processCancel(appUser);
+        } else if (UserState.BASIC_STATE.equals(userState)) {
+            output = processServiceCommand(appUser, text);
+        } else if (UserState.WAIT_FOR_EMAIL_STATE.equals(userState)) {
+            // TODO add processing of the email
+        } else {
+            log.error("Unknown state: " + userState);
+            output = "Unknown error! Please, input /cancel and try again.";
+        }
+
+        var chatId = update.getMessage().getChatId();
+        sendAnswer(output, chatId);
+
+
+    }
+
+    @Override
+    public void processDocMessage(Update update) {
+        saveRawData(update);
+
+        var appUser = findOrSaveAppUser(update);
+        var chatId = update.getMessage().getChatId();
+        if (isNotAllowedToSendContent(chatId, appUser)) {
+            return;
+        }
+        // TODO add logic to save document
+        var answer = "Document saved successfully! Use link to download: http://test.ru/get-doc/777";
+        sendAnswer(answer, chatId);
+    }
+
+    @Override
+    public void processPhotoMessage(Update update) {
+        saveRawData(update);
+
+        var appUser = findOrSaveAppUser(update);
+        var chatId = update.getMessage().getChatId();
+        if (isNotAllowedToSendContent(chatId, appUser)) {
+            return;
+        }
+        // TODO add logic to save photo
+        var answer = "Photo saved successfully! Use link to download: http://test.ru/get-doc/777";
+        sendAnswer(answer, chatId);
+    }
+
+
+    private boolean isNotAllowedToSendContent(Long chatId, AppUser appUser) {
+        var userState = appUser.getState();
+        if (!appUser.getIsActive()) {
+            var error = "Register or activate your account to load content.";
+            sendAnswer(error, chatId);
+            return true;
+        } else if (!UserState.BASIC_STATE.equals(userState)) {
+            var error = "Cancel current command with the help of /cancel to load files.";
+            sendAnswer(error, chatId);
+            return true;
+        }
+        return false;
+    }
+
+    private void sendAnswer(String output, Long chatId) {
         var sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Hello from NODE");
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(output);
         producerService.produceAnswer(sendMessage);
     }
 
-    private AppUser findOrSaveAppUser(User telegramUser) {
+    private String processServiceCommand(AppUser appUser, String cmd) {
+        if (ServiceCommands.REGISTRATION.equals(cmd)) {
+            // TODO add registration
+            return "Temporarily unavailable";
+        } else if (ServiceCommands.HELP.equals(cmd)) {
+            return help();
+        } else if (ServiceCommands.START.equals(cmd)) {
+            return "Hi! To get a list of available commands, input /help";
+        } else {
+            return "Unknown command! To get a list of available commands, input /help";
+        }
+    }
+
+    private String help() {
+        return "List of available commands:\n" +
+                "/cancel - cancel current command\n" +
+                "/registration - register new user.";
+    }
+
+    private String processCancel(AppUser appUser) {
+        appUser.setState(UserState.BASIC_STATE);
+        appUserDAO.save(appUser);
+        return "Command cancelled";
+    }
+
+    private AppUser findOrSaveAppUser(Update update) {
+        var textMessage = update.getMessage();
+        var telegramUser = textMessage.getFrom();
         AppUser persistentAppUser = appUserDAO.findAppUserByTelegramUserId(telegramUser.getId());
         if (persistentAppUser == null) {
             AppUser transientAppUser = AppUser.builder()
